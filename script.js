d2e374f76480dd9d2368db325bc5dfa56b839d53
AliHussein <alihusseiinali@gmail.com>
2022-12-04 16:47:41 +0200

    script added

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 "use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnShowModal = document.querySelectorAll(".show-modal");
+const btnCloseModal = document.querySelector(".close-modal");
+const openTheModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const clostTheModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener("click", openTheModal);
+  btnCloseModal.addEventListener("click", clostTheModal);
+  overlay.addEventListener("click", clostTheModal);
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden"))
+      clostTheModal();
+  });
+}
